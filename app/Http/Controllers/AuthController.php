cdf2a7195941c84ec41ab939270179bfef464de2
daruadi <pintoko.mpd@gmail.com>
2026-01-28 11:02:49 +0700

    Refactor AuthController to use route redirects

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,13 +24,13 @@ class AuthController extends Controller
             'username' => ['required'],
             'password' => ['required'],
         ]);
-
+    
         if (Auth::attempt($credentials, $request->boolean('remember'))) {
             $request->session()->regenerate();
-
-            return redirect()->intended('dashboard');
+    
+            return redirect()->route('dashboard');
         }
-
+    
         return back()->withErrors([
             'username' => 'The provided credentials do not match our records.',
         ])->onlyInput('username');
@@ -42,11 +42,10 @@ class AuthController extends Controller
     public function logout(Request $request)
     {
         Auth::logout();
-
+    
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
-
-        return redirect('/login');
+    
+        return redirect()->route('login');
     }
 }
